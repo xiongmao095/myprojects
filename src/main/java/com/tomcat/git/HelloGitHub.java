b537e1f37aa04a67a563d548e7815adc773c05b0
xiongmao095 <3100821189@qq.com>
2020-12-01 20:33:08 +0800

    测试多人协同

--- src/main/java/com/tomcat/git/HelloGitHub.java
+++ src/main/java/com/tomcat/git/HelloGitHub.java
@@ -5,6 +5,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("通过github克隆的项目添加的内容");
+		System.out.println("通过github克隆的项目添加的内容，测试多人协同");
 	}
 
 }
